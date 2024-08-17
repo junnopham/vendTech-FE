1cf6f7ff7c456b0d694b79fc2295b3291a3438e3
Phạm Đình Sơn <sonpd4@fpt.com>
2024-08-17 21:44:45 +0700

    [FE] Format code

--- src/Page/Admin/Category/index.tsx
+++ src/Page/Admin/Category/index.tsx
@@ -5,201 +5,213 @@ import { GetProp, Modal, notification, Table, TableProps } from 'antd';
 import type { SorterResult } from 'antd/es/table/interface';
 import { formatTime } from '../../../util/date-time';
 import {
-  DeleteOutlined,
-  EditOutlined,
-  ExclamationCircleFilled,
+	DeleteOutlined,
+	EditOutlined,
+	ExclamationCircleFilled,
 } from '@ant-design/icons';
 import {
-  deleteCategoryById,
-  getCategories,
+	deleteCategoryById,
+	getCategories,
 } from '../../../service/category.service';
 import EditCategoryModal from '../../../Components/Admin/EditCategoryModal';
 
 const { confirm } = Modal;
 
 const Category = () => {
-  useTitle('Category');
-  type ColumnsType<T> = TableProps<T>['columns'];
-  type TablePaginationConfig = Exclude<
-    GetProp<TableProps, 'pagination'>,
-    boolean
-  >;
+	useTitle('Category');
+	type ColumnsType<T> = TableProps<T>['columns'];
+	type TablePaginationConfig = Exclude<
+		GetProp<TableProps, 'pagination'>,
+		boolean
+	>;
 
-  interface TableParams {
-    pagination?: TablePaginationConfig;
-    sortField?: SorterResult<any>['field'];
-    sortOrder?: SorterResult<any>['order'];
-    filters?: Parameters<GetProp<TableProps, 'onChange'>>[1];
-  }
+	interface TableParams {
+		pagination?: TablePaginationConfig;
+		sortField?: SorterResult<any>['field'];
+		sortOrder?: SorterResult<any>['order'];
+		filters?: Parameters<GetProp<TableProps, 'onChange'>>[1];
+	}
 
-  const columns: ColumnsType<Category> = [
-    {
-      title: 'Index',
-      key: 'index',
-      render: (text, record, index) => index + 1,
-    },
-    {
-      title: 'Name',
-      dataIndex: 'name',
-    },
-    {
-      title: 'Created At',
-      dataIndex: 'createdAt',
-      render: (text, record, index) => formatTime(record.createdAt),
-    },
-    {
-      title: 'Updated At',
-      dataIndex: 'updatedAt',
-      render: (text, record, index) => formatTime(record.updatedAt),
-    },
-    {
-      title: 'Action',
-      dataIndex: 'action',
-      width: '10%',
-      render: (text, record, index) => {
-        return (
-          <div className="action-row">
-            <EditOutlined
-              onClick={(event) => handleEdit(record)}
-              style={{ fontSize: '16px', cursor: 'pointer' }}
-            />
-            <DeleteOutlined
-              onClick={(event) => handleDelete(record)}
-              style={{ fontSize: '16px', cursor: 'pointer' }}
-            />
-          </div>
-        );
-      },
-    },
-  ];
+	const columns: ColumnsType<Category> = [
+		{
+			title: 'Index',
+			key: 'index',
+			render: (text, record, index) => index + 1,
+		},
+		{
+			title: 'Name',
+			dataIndex: 'name',
+		},
+		{
+			title: 'Created At',
+			dataIndex: 'createdAt',
+			render: (text, record, index) => formatTime(record.createdAt),
+		},
+		{
+			title: 'Updated At',
+			dataIndex: 'updatedAt',
+			render: (text, record, index) => formatTime(record.updatedAt),
+		},
+		{
+			title: 'Action',
+			dataIndex: 'action',
+			width: '10%',
+			render: (text, record, index) => {
+				return (
+					<div className="action-row">
+						<EditOutlined
+							onClick={(event) => handleEdit(record)}
+							style={{ fontSize: '16px', cursor: 'pointer' }}
+						/>
+						<DeleteOutlined
+							onClick={(event) => handleDelete(record)}
+							style={{ fontSize: '16px', cursor: 'pointer' }}
+						/>
+					</div>
+				);
+			},
+		},
+	];
 
-  const [data, setData] = useState<Category[]>();
-  const [loading, setLoading] = useState(false);
-  const [reload, setReload] = useState(false);
-  const [tableParams, setTableParams] = useState<TableParams>({
-    pagination: {
-      current: 1,
-      pageSize: 10,
-    },
-  });
-  const [category, setCategory] = useState<Category | null>(null);
+	const [data, setData] = useState<Category[]>();
+	const [loading, setLoading] = useState(false);
+	const [reload, setReload] = useState(false);
+	const [tableParams, setTableParams] = useState<TableParams>({
+		pagination: {
+			current: 1,
+			pageSize: 10,
+		},
+	});
+	const [category, setCategory] = useState<Category | null>(null);
 
-  const [api, contextHolder] = notification.useNotification();
-  type NotificationType = 'success' | 'info' | 'warning' | 'error';
-  const openNotificationWithIcon = (
-    type: NotificationType,
-    message?: string,
-    description?: string
-  ) => {
-    api[type]({
-      message,
-      description,
-    });
-  };
+	const [api, contextHolder] = notification.useNotification();
+	type NotificationType = 'success' | 'info' | 'warning' | 'error';
+	const openNotificationWithIcon = (
+		type: NotificationType,
+		message?: string,
+		description?: string
+	) => {
+		api[type]({
+			message,
+			description,
+		});
+	};
 
-  const handleEdit = (record: Category) => {
-    setCategory(record);
-  };
+	const handleEdit = (record: Category) => {
+		setCategory(record);
+	};
 
-  const handleDelete = (record: Category) => {
-    confirm({
-      title: 'Are you sure delete this product?',
-      icon: <ExclamationCircleFilled />,
-      okText: 'Yes',
-      okType: 'danger',
-      cancelText: 'No',
-      onOk() {
-        deleteCategoryById(record._id)
-          .then((res) => {
-            setReload(!reload);
-            const deleteInfo: { type: NotificationType; message: string } = {
-              type: 'success',
-              message: 'Delete successfully!',
-            };
-            openNotificationWithIcon(deleteInfo.type, deleteInfo.message);
-          })
-          .catch((err) => {
-            const deleteInfo: { type: NotificationType; message: string } = {
-              type: 'error',
-              message: 'Delete fail!',
-            };
-            openNotificationWithIcon(deleteInfo.type, deleteInfo.message);
-          });
-      },
-    });
-  };
+	const handleDelete = (record: Category) => {
+		confirm({
+			title: 'Are you sure delete this product?',
+			icon: <ExclamationCircleFilled />,
+			okText: 'Yes',
+			okType: 'danger',
+			cancelText: 'No',
+			onOk() {
+				deleteCategoryById(record._id)
+					.then((res) => {
+						setReload(!reload);
+						const deleteInfo: {
+							type: NotificationType;
+							message: string;
+						} = {
+							type: 'success',
+							message: 'Delete successfully!',
+						};
+						openNotificationWithIcon(
+							deleteInfo.type,
+							deleteInfo.message
+						);
+					})
+					.catch((err) => {
+						const deleteInfo: {
+							type: NotificationType;
+							message: string;
+						} = {
+							type: 'error',
+							message: 'Delete fail!',
+						};
+						openNotificationWithIcon(
+							deleteInfo.type,
+							deleteInfo.message
+						);
+					});
+			},
+		});
+	};
 
-  const fetchData = () => {
-    setLoading(true);
-    getCategories(
-      tableParams.pagination?.pageSize || 10,
-      tableParams.pagination?.current || 0
-    ).then((results) => {
-      setData(results.data);
-      setLoading(false);
-      setTableParams({
-        ...tableParams,
-        pagination: {
-          ...tableParams.pagination,
-          showSizeChanger: true,
-          pageSizeOptions: ['10', '20', '30', '40', '50'],
-          total: results.total,
-        },
-      });
-    });
-  };
+	const fetchData = () => {
+		setLoading(true);
+		getCategories(
+			tableParams.pagination?.pageSize || 10,
+			tableParams.pagination?.current || 0
+		).then((results) => {
+			setData(results.data);
+			setLoading(false);
+			setTableParams({
+				...tableParams,
+				pagination: {
+					...tableParams.pagination,
+					showSizeChanger: true,
+					pageSizeOptions: ['10', '20', '30', '40', '50'],
+					total: results.total,
+				},
+			});
+		});
+	};
 
-  useEffect(() => {
-    fetchData();
-  }, [
-    tableParams.pagination?.current,
-    tableParams.pagination?.pageSize,
-    reload,
-  ]);
+	useEffect(() => {
+		fetchData();
+	}, [
+		tableParams.pagination?.current,
+		tableParams.pagination?.pageSize,
+		reload,
+	]);
 
-  const handleTableChange: TableProps['onChange'] = (pagination) => {
-    setTableParams({
-      pagination,
-    });
-    if (pagination.pageSize !== tableParams.pagination?.pageSize) {
-      setData([]);
-    }
-  };
+	const handleTableChange: TableProps['onChange'] = (pagination) => {
+		setTableParams({
+			pagination,
+		});
+		if (pagination.pageSize !== tableParams.pagination?.pageSize) {
+			setData([]);
+		}
+	};
 
-  const handleUpdate = (updatedCategory: Category) => {
-    setData(
-      (prevData) =>
-        prevData?.map((item) =>
-          item._id === updatedCategory._id ? updatedCategory : item
-        ) || []
-    );
-  };
+	const handleUpdate = (updatedCategory: Category) => {
+		setData(
+			(prevData) =>
+				prevData?.map((item) =>
+					item._id === updatedCategory._id ? updatedCategory : item
+				) || []
+		);
+	};
 
-  const onCloseModal = () => {
-    setCategory(null);
-  };
+	const onCloseModal = () => {
+		setCategory(null);
+	};
 
-  return (
-    <>
-      {contextHolder}
-      <Table
-        scroll={{ x: 768 }}
-        columns={columns}
-        rowKey={(record) => record._id}
-        dataSource={data}
-        pagination={tableParams.pagination}
-        loading={loading}
-        onChange={handleTableChange}
-      />
-      {category && (
-        <EditCategoryModal
-          category={category}
-          onClose={onCloseModal}
-          onUpdate={handleUpdate}
-        />
-      )}
-    </>
-  );
+	return (
+		<>
+			{contextHolder}
+			<Table
+				scroll={{ x: 768 }}
+				columns={columns}
+				rowKey={(record) => record._id}
+				dataSource={data}
+				pagination={tableParams.pagination}
+				loading={loading}
+				onChange={handleTableChange}
+			/>
+			{category && (
+				<EditCategoryModal
+					category={category}
+					onClose={onCloseModal}
+					onUpdate={handleUpdate}
+				/>
+			)}
+		</>
+	);
 };
 
 export default Category;
--- src/Page/Admin/GuestInfo/index.tsx
+++ src/Page/Admin/GuestInfo/index.tsx
@@ -5,8 +5,8 @@ import type { SorterResult } from 'antd/es/table/interface';
 import { formatTime } from '../../../util/date-time';
 import { DeleteOutlined, ExclamationCircleFilled } from '@ant-design/icons';
 import {
-  deleteGuestInfo,
-  getGuestInfo,
+	deleteGuestInfo,
+	getGuestInfo,
 } from '../../../service/guestinfo.service';
 import withAuth from '../../../Components/Admin/withAuth';
 
@@ -15,175 +15,187 @@ import './style.css';
 const { confirm } = Modal;
 
 const GuestInfo = () => {
-  useTitle('Guest Info');
-  type ColumnsType<T> = TableProps<T>['columns'];
-  type TablePaginationConfig = Exclude<
-    GetProp<TableProps, 'pagination'>,
-    boolean
-  >;
+	useTitle('Guest Info');
+	type ColumnsType<T> = TableProps<T>['columns'];
+	type TablePaginationConfig = Exclude<
+		GetProp<TableProps, 'pagination'>,
+		boolean
+	>;
 
-  interface TableParams {
-    pagination?: TablePaginationConfig;
-    sortField?: SorterResult<any>['field'];
-    sortOrder?: SorterResult<any>['order'];
-    filters?: Parameters<GetProp<TableProps, 'onChange'>>[1];
-  }
+	interface TableParams {
+		pagination?: TablePaginationConfig;
+		sortField?: SorterResult<any>['field'];
+		sortOrder?: SorterResult<any>['order'];
+		filters?: Parameters<GetProp<TableProps, 'onChange'>>[1];
+	}
 
-  const columns: ColumnsType<GuestInfo> = [
-    {
-      title: 'ID',
-      key: 'index',
-      render: (text, record, index) => index + 1,
-    },
-    {
-      title: 'First Name',
-      dataIndex: 'firstName',
-    },
-    {
-      title: 'Last Name',
-      dataIndex: 'lastName',
-    },
-    {
-      title: 'Email',
-      dataIndex: 'email',
-    },
-    {
-      title: 'Phone Number',
-      dataIndex: 'phoneNumber',
-    },
-    {
-      title: 'Message',
-      dataIndex: 'message',
-    },
-    {
-      title: 'Created At',
-      dataIndex: 'createdAt',
-      render: (text, record, index) => formatTime(record.createdAt),
-    },
-    {
-      title: 'Updated At',
-      dataIndex: 'updatedAt',
-      render: (text, record, index) => formatTime(record.updatedAt),
-    },
-    {
-      title: 'Action',
-      dataIndex: 'action',
-      width: '10%',
-      render: (text, record, index) => {
-        return (
-          <div className="action-row">
-            <DeleteOutlined
-              onClick={(event) => handleDelete(record)}
-              style={{ fontSize: '16px', cursor: 'pointer' }}
-            />
-          </div>
-        );
-      },
-    },
-  ];
+	const columns: ColumnsType<GuestInfo> = [
+		{
+			title: 'ID',
+			key: 'index',
+			render: (text, record, index) => index + 1,
+		},
+		{
+			title: 'First Name',
+			dataIndex: 'firstName',
+		},
+		{
+			title: 'Last Name',
+			dataIndex: 'lastName',
+		},
+		{
+			title: 'Email',
+			dataIndex: 'email',
+		},
+		{
+			title: 'Phone Number',
+			dataIndex: 'phoneNumber',
+		},
+		{
+			title: 'Message',
+			dataIndex: 'message',
+		},
+		{
+			title: 'Created At',
+			dataIndex: 'createdAt',
+			render: (text, record, index) => formatTime(record.createdAt),
+		},
+		{
+			title: 'Updated At',
+			dataIndex: 'updatedAt',
+			render: (text, record, index) => formatTime(record.updatedAt),
+		},
+		{
+			title: 'Action',
+			dataIndex: 'action',
+			width: '10%',
+			render: (text, record, index) => {
+				return (
+					<div className="action-row">
+						<DeleteOutlined
+							onClick={(event) => handleDelete(record)}
+							style={{ fontSize: '16px', cursor: 'pointer' }}
+						/>
+					</div>
+				);
+			},
+		},
+	];
 
-  const [data, setData] = useState<GuestInfo[]>();
-  const [loading, setLoading] = useState(false);
-  const [reload, setReload] = useState(false);
-  const [tableParams, setTableParams] = useState<TableParams>({
-    pagination: {
-      current: 1,
-      pageSize: 10,
-    },
-  });
+	const [data, setData] = useState<GuestInfo[]>();
+	const [loading, setLoading] = useState(false);
+	const [reload, setReload] = useState(false);
+	const [tableParams, setTableParams] = useState<TableParams>({
+		pagination: {
+			current: 1,
+			pageSize: 10,
+		},
+	});
 
-  const [api, contextHolder] = notification.useNotification();
-  type NotificationType = 'success' | 'info' | 'warning' | 'error';
-  const openNotificationWithIcon = (
-    type: NotificationType,
-    message?: string,
-    description?: string
-  ) => {
-    api[type]({
-      message,
-      description,
-    });
-  };
+	const [api, contextHolder] = notification.useNotification();
+	type NotificationType = 'success' | 'info' | 'warning' | 'error';
+	const openNotificationWithIcon = (
+		type: NotificationType,
+		message?: string,
+		description?: string
+	) => {
+		api[type]({
+			message,
+			description,
+		});
+	};
 
-  const handleDelete = (record: GuestInfo) => {
-    confirm({
-      title: 'Are you sure delete this guest?',
-      icon: <ExclamationCircleFilled />,
-      okText: 'Yes',
-      okType: 'danger',
-      cancelText: 'No',
-      onOk() {
-        deleteGuestInfo(record._id)
-          .then((res) => {
-            setReload(!reload);
-            const deleteInfo: { type: NotificationType; message: string } = {
-              type: 'success',
-              message: 'Delete successfully!',
-            };
-            openNotificationWithIcon(deleteInfo.type, deleteInfo.message);
-          })
-          .catch((err) => {
-            const deleteInfo: { type: NotificationType; message: string } = {
-              type: 'error',
-              message: 'Delete fail!',
-            };
-            openNotificationWithIcon(deleteInfo.type, deleteInfo.message);
-          });
-      },
-    });
-  };
+	const handleDelete = (record: GuestInfo) => {
+		confirm({
+			title: 'Are you sure delete this guest?',
+			icon: <ExclamationCircleFilled />,
+			okText: 'Yes',
+			okType: 'danger',
+			cancelText: 'No',
+			onOk() {
+				deleteGuestInfo(record._id)
+					.then((res) => {
+						setReload(!reload);
+						const deleteInfo: {
+							type: NotificationType;
+							message: string;
+						} = {
+							type: 'success',
+							message: 'Delete successfully!',
+						};
+						openNotificationWithIcon(
+							deleteInfo.type,
+							deleteInfo.message
+						);
+					})
+					.catch((err) => {
+						const deleteInfo: {
+							type: NotificationType;
+							message: string;
+						} = {
+							type: 'error',
+							message: 'Delete fail!',
+						};
+						openNotificationWithIcon(
+							deleteInfo.type,
+							deleteInfo.message
+						);
+					});
+			},
+		});
+	};
 
-  const fetchData = () => {
-    setLoading(true);
-    getGuestInfo(
-      tableParams.pagination?.pageSize || 10,
-      tableParams.pagination?.current || 0
-    ).then((results) => {
-      setData(results.data);
-      setLoading(false);
-      setTableParams({
-        ...tableParams,
-        pagination: {
-          ...tableParams.pagination,
-          showSizeChanger: true,
-          pageSizeOptions: ['10', '20', '30', '40', '50'],
-          total: results.total,
-        },
-      });
-    });
-  };
+	const fetchData = () => {
+		setLoading(true);
+		getGuestInfo(
+			tableParams.pagination?.pageSize || 10,
+			tableParams.pagination?.current || 0
+		).then((results) => {
+			setData(results.data);
+			setLoading(false);
+			setTableParams({
+				...tableParams,
+				pagination: {
+					...tableParams.pagination,
+					showSizeChanger: true,
+					pageSizeOptions: ['10', '20', '30', '40', '50'],
+					total: results.total,
+				},
+			});
+		});
+	};
 
-  useEffect(() => {
-    fetchData();
-  }, [
-    tableParams.pagination?.current,
-    tableParams.pagination?.pageSize,
-    reload,
-  ]);
+	useEffect(() => {
+		fetchData();
+	}, [
+		tableParams.pagination?.current,
+		tableParams.pagination?.pageSize,
+		reload,
+	]);
 
-  const handleTableChange: TableProps['onChange'] = (pagination) => {
-    setTableParams({
-      pagination,
-    });
-    if (pagination.pageSize !== tableParams.pagination?.pageSize) {
-      setData([]);
-    }
-  };
+	const handleTableChange: TableProps['onChange'] = (pagination) => {
+		setTableParams({
+			pagination,
+		});
+		if (pagination.pageSize !== tableParams.pagination?.pageSize) {
+			setData([]);
+		}
+	};
 
-  return (
-    <>
-      {contextHolder}
-      <Table
-        scroll={{ x: 768 }}
-        columns={columns}
-        rowKey={(record) => record._id}
-        dataSource={data}
-        pagination={tableParams.pagination}
-        loading={loading}
-        onChange={handleTableChange}
-      />
-    </>
-  );
+	return (
+		<>
+			{contextHolder}
+			<Table
+				scroll={{ x: 768 }}
+				columns={columns}
+				rowKey={(record) => record._id}
+				dataSource={data}
+				pagination={tableParams.pagination}
+				loading={loading}
+				onChange={handleTableChange}
+			/>
+		</>
+	);
 };
 
 export default withAuth(GuestInfo);
